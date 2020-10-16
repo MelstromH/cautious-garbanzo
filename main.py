4b5d0a0462df74acee2df70359efbccb2f9a97c8
MelstromH <32555061+MelstromH@users.noreply.github.com>
2020-10-16 17:00:36 -0500

    First try

---
+++ main.py
@@ -0,0 +1,29 @@
+
+fb = 0
+for i in range(100):
+    if i % 3 == 0:
+        fb = 1
+
+    if i % 5 == 0 & fb == 1:
+        fb = 3
+
+    if i % 5 == 0 & fb != 3:
+        fb = 2
+
+    if fb == 1:
+        print("Fizz")
+
+    if fb == 2:
+        print("Buzz")
+
+    if fb == 3:
+        print("Fizz Buzz")
+
+    if fb == 0:
+        print(i)
+
+    fb = 0
+
+
+
+
